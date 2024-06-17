8e1aa6d6d51e8129f2b4afe7c2e29a9cd52b91a8
josh brown <jbro463@gmail.com>
2024-06-17 13:09:42 -0400

    push pair project steps 1-5

--- step-5.sql
+++ step-5.sql
@@ -9,14 +9,21 @@
   description: Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.
   ------------------------------
 */
+INSERT INTO park (name, location, establish_date, area, visitors, description)
+VALUES ('Ohiopyle State Park', 'Pennsylvania', '1965-01-01', 19052, 1000000, 'Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.
+' );
 
 
 /*
   STEP TWO: You just found out that there was an error with the park data. Please update the park visitors to 1.5 million anually.
 
-*/
+*/ 
+	UPDATE park
+    SET visitors = visitors + 500000
+    WHERE name = 'Ohiopyle State Park';
 
-
+SELECT * FROM PARK
+WHERE name = 'Ohiopyle State Park'
 /*
  STEP THREE: Insert new campground with the following data:
   ------------------------------------------------------------
@@ -27,8 +34,11 @@
   daily_fee: 95.00
   ------------------------------------------------------------
 */
+INSERT INTO campground ( park_id, name, open_from_mm, open_to_mm, daily_fee)
+VALUES (5, 'Youghiogheny', 01,12, 95.00)
 
-
+SELECT * FROM campground 
+WHERE name = 'Youghiogheny'
 /*
  STEP FOUR: Insert 3 new sites with the following data:
  ------------------------------------------------------------
@@ -37,8 +47,11 @@
   site_number: 625, campground_id: (use the id of the new campground you just added)
  ------------------------------------------------------------
 */
+INSERT INTO site (site_number, campground_id)
+VALUES (623, 8),(624,8),(625,8);
 
-
+SELECT * FROM site 
+WHERE campground_id = 8
 /*
  STEP FIVE: Insert 3 reservations, 1 for each site with the following data:
 ------------------------------------------------------------------------------------
@@ -47,7 +60,8 @@
   site_id: (the site_id for site number 625 that you just created), name: Kent Family, from_date: today + 12 days, to_date: today + 20 days
 ------------------------------------------------------------------------------------
 */
-
+INSERT INTO reservation (site_id, name, from_date, to_date)
+VALUES (52, 'Wayne Family', date ('2024-26-6'), date ('2024-6-7')  ), (53, 'Parker Family', date ('2024-27-6'), date ('2024-6-7') ), (54, 'Kent Family', date ('2024-28-6'), date ('2024-6-7') );
 
 /*
  STEP SIX: The Wayne Family called and asked if they could change their reservation to today. Update the from_date to today and the to_date to a week from today.
@@ -58,5 +72,7 @@
 /*
  STEP SEVEN: The Kent family called and they would like to cancel their reservation. Delete the reservation for Kent Family.
 
-*/
+*/ 
+DELETE FROM reservation
+WHERE name = 'Kent Family'
 
